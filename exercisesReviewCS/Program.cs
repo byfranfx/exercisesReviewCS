217fd9b7a7f43f33c672351a02c8705955411021
byfranfx <71705230+byfranfx@users.noreply.github.com>
2020-10-16 20:11:22 +0200

    end class

--- exercisesReviewCS/Program.cs
+++ exercisesReviewCS/Program.cs
@@ -10,13 +10,43 @@ namespace exercisesReviewCS
     {
         static void Main(string[] args)
         {
-            string[] nombres = { "Alberto", "Andrés", "Antonio" };
+            string[] nombres = { "John", "Mary", "Sue", "Greg", "Yolanda", "Jose", "Bill" };
             foreach (string nombre in nombres)
             {
                 Console.WriteLine("Hello {0}", nombre);
             }
             Console.WriteLine();
             Console.ReadKey();
+
+            // class PersonModel
+
+            PersonModel person = new PersonModel("wdc", "wdc");
+            Console.WriteLine(" {0} {1}", person.FirstName, person.LastName);
+
+            
+
         }
     }
+
+    /*
+     * Person person1 = new Person("Leopold", 6);
+        Console.WriteLine("person1 Name = {0} Age = {1}", person1.Name, person1.Age);
+
+        // Declare new person, assign person1 to it.
+        Person person2 = person1;
+
+        // Change the name of person2, and person1 also changes.
+        person2.Name = "Molly";
+        person2.Age = 16;
+
+        Console.WriteLine("person2 Name = {0} Age = {1}", person2.Name, person2.Age);
+        Console.WriteLine("person1 Name = {0} Age = {1}", person1.Name, person1.Age);
+
+        // Keep the console open in debug mode.
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+     * 
+     * */
+
+
 }
