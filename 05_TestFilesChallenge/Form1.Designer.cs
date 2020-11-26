ce909f614b8dd3bb8b5d612b353447a9d17a163b
byfranfx <71705230+byfranfx@users.noreply.github.com>
2020-11-26 13:49:46 +0100

    exercisesReviewCS : 05_TestFileChallenge
    completed with boolean error.

---
+++ 05_TestFilesChallenge/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace _05_TestFilesChallenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usersListBox = new System.Windows.Forms.ListBox();
+            this.addUserButton = new System.Windows.Forms.Button();
+            this.saveListButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.firstNameText = new System.Windows.Forms.TextBox();
+            this.lastNametext = new System.Windows.Forms.TextBox();
+            this.isAliveCheckBox = new System.Windows.Forms.CheckBox();
+            this.agePicker = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.agePicker)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // usersListBox
+            // 
+            this.usersListBox.FormattingEnabled = true;
+            this.usersListBox.Location = new System.Drawing.Point(462, 55);
+            this.usersListBox.Name = "usersListBox";
+            this.usersListBox.Size = new System.Drawing.Size(326, 381);
+            this.usersListBox.TabIndex = 0;
+            // 
+            // addUserButton
+            // 
+            this.addUserButton.Location = new System.Drawing.Point(180, 273);
+            this.addUserButton.Name = "addUserButton";
+            this.addUserButton.Size = new System.Drawing.Size(75, 23);
+            this.addUserButton.TabIndex = 1;
+            this.addUserButton.Text = "AddUser";
+            this.addUserButton.UseVisualStyleBackColor = true;
+            this.addUserButton.Click += new System.EventHandler(this.addUserButton_Click);
+            // 
+            // saveListButton
+            // 
+            this.saveListButton.Location = new System.Drawing.Point(180, 316);
+            this.saveListButton.Name = "saveListButton";
+            this.saveListButton.Size = new System.Drawing.Size(75, 23);
+            this.saveListButton.TabIndex = 2;
+            this.saveListButton.Text = "SaveUser";
+            this.saveListButton.UseVisualStyleBackColor = true;
+            this.saveListButton.Click += new System.EventHandler(this.saveListButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(220, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "First name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Last name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Age:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Is Alive:";
+            // 
+            // firstNameText
+            // 
+            this.firstNameText.Location = new System.Drawing.Point(180, 106);
+            this.firstNameText.Name = "firstNameText";
+            this.firstNameText.Size = new System.Drawing.Size(100, 20);
+            this.firstNameText.TabIndex = 8;
+            // 
+            // lastNametext
+            // 
+            this.lastNametext.Location = new System.Drawing.Point(180, 141);
+            this.lastNametext.Name = "lastNametext";
+            this.lastNametext.Size = new System.Drawing.Size(100, 20);
+            this.lastNametext.TabIndex = 9;
+            // 
+            // isAliveCheckBox
+            // 
+            this.isAliveCheckBox.AutoSize = true;
+            this.isAliveCheckBox.Location = new System.Drawing.Point(180, 201);
+            this.isAliveCheckBox.Name = "isAliveCheckBox";
+            this.isAliveCheckBox.Size = new System.Drawing.Size(80, 17);
+            this.isAliveCheckBox.TabIndex = 10;
+            this.isAliveCheckBox.Text = "checkBox1";
+            this.isAliveCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // agePicker
+            // 
+            this.agePicker.Location = new System.Drawing.Point(180, 175);
+            this.agePicker.Name = "agePicker";
+            this.agePicker.Size = new System.Drawing.Size(100, 20);
+            this.agePicker.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.agePicker);
+            this.Controls.Add(this.isAliveCheckBox);
+            this.Controls.Add(this.lastNametext);
+            this.Controls.Add(this.firstNameText);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveListButton);
+            this.Controls.Add(this.addUserButton);
+            this.Controls.Add(this.usersListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.agePicker)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox usersListBox;
+        private System.Windows.Forms.Button addUserButton;
+        private System.Windows.Forms.Button saveListButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox firstNameText;
+        private System.Windows.Forms.TextBox lastNametext;
+        private System.Windows.Forms.CheckBox isAliveCheckBox;
+        private System.Windows.Forms.NumericUpDown agePicker;
+    }
+}
+
---
+++ 05_TestFilesChallenge/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05_TestFilesChallenge
+{
+    public partial class Form1 : Form
+    {
+
+        BindingList<UserModel> users = new BindingList<UserModel>();
+        int firstNameOrder = 0;
+        int lastNameOrder = 0;
+        int ageOrder = 0;
+        int isAliveOrder = 0;
+        string fileName = "StandardDataSet.csv";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LoadListFormFile();
+
+            WireUpDropDown();
+        }
+
+        public void LoadListFormFile()
+        {
+            string[] lines = File.ReadAllLines(fileName);
+            string[] headers = lines[0].Split(',');
+
+            for(int i = 0; i < headers.Length; i++)
+            {
+                if (headers[i] == "FirstName")
+                {
+                    firstNameOrder = i;
+                } 
+                else if (headers[i] == "Lastname")
+                {
+                    lastNameOrder = i;
+                }
+                else if (headers[i] == "Age")
+                {
+                    ageOrder = i;
+                }
+                else if (headers[i] == "Isalive")
+                {
+                    isAliveOrder = i;
+                }
+            }
+
+            for(int i = 1; i < lines.Length; i++)
+            {
+                string[] columns = lines[i].Split(',');
+                bool isAlive = false;
+
+                if(columns[isAliveOrder] == "1")
+                {
+                    isAlive = true;
+                }
+
+                users.Add(new UserModel
+                {
+                    FirstName = columns[firstNameOrder],
+                    LastName = columns[lastNameOrder],
+                    Age = int.Parse(columns[ageOrder]),
+                    //IsAlive = bool.Parse(columns[isAliveOrder])
+                });
+
+            }
+        }
+
+        private void WireUpDropDown()
+        {
+            usersListBox.DataSource = users;
+            usersListBox.DisplayMember = nameof(UserModel.DisplayText);
+
+        }
+
+        private void addUserButton_Click(object sender, EventArgs e)
+        {
+            users.Add(new UserModel
+            {
+                FirstName = firstNameText.Text,
+                LastName = lastNametext.Text,
+                Age = (int)agePicker.Value,
+                IsAlive = isAliveCheckBox.Checked
+            });
+
+            firstNameText.Text = "";
+            lastNametext.Text = "";
+            agePicker.Value = 0;
+            isAliveCheckBox.Checked = false;
+        }
+
+        private void saveListButton_Click(object sender, EventArgs e)
+        {
+            List<string> lines = new List<string>();
+            lines.Add("FirstName, Lastname, Age, IsAlive");
+
+            foreach(UserModel user in users)
+            {
+                int isAliveValue = 0;
+                if(user.IsAlive == true)
+                {
+                    isAliveValue = 1;
+                }
+
+                lines.Add($"{ user.FirstName }, { user.LastName },{ user.Age },{ user.IsAlive }");
+            }
+            File.WriteAllLines("StandardDataSet.csv", lines);
+            MessageBox.Show("Save Completed");
+        }
+    }
+}
